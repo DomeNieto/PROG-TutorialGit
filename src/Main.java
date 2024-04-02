0a86f78684d787d51254e2b54b128722b31a4ca9
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2024-04-02 12:21:49 +0100

    feat(Main): ask number to user and print it

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main{
 public static void main(String[] args) {
-    System.out.println("Hola, Mundo!");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Ingrese un numero");
+    int askNumber = scanner.nextInt();
+
+    System.out.println("Hola, Mundo!. Tu numero es: " + askNumber);
+
+    scanner.close();
 }
 }
